f8bdbe7974891414d406c498abaa14db58c973d8
David Nowakowski <dnowakowski@neonomics.io>
2023-04-25 11:02:01 +0200

    feat: Initial commit

---
+++ src/main/java/com/dns/pactconsumer/PactConsumer2Application.java
@@ -0,0 +1,13 @@
+package com.dns.pactconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactConsumer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactConsumer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dns/pactconsumer/PactConsumer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dns.pactconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactConsumer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
